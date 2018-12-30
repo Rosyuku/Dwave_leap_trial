464e15a5a71f38287f5203f97b31efeedccc03db
linasugi <sogenakotoga@hotmail.com>
2018-12-30 20:18:46 +0900

    不等式制約の考え方を緩和したところ、それなりに解けるようになった。N=15までは全探索と一致した解。検討数を増やすことが肝心。

---
+++ knapsack problem/quantum_case2.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 30 09:34:09 2018
+
+@author: kazuyuki
+"""
+
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+import setting
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+import time
+
+start = time.time()
+np.random.seed(2)
+
+alpha = 1
+detail = False
+
+n = 22
+weight_limit = n * 2
+margin = 2
+
+value = np.random.randint(1, n, n)
+weight = np.random.randint(1, n, n)
+c = np.array([-weight_limit]+[1, 2])
+
+Q = dict()
+for i in range(n+margin+1):
+    for j in range(n+margin+1):
+        if i==j:
+            if i < n:
+                Q.update({("x"+str(i), "x"+str(i)):-value[i]+alpha*weight[i]**2})
+            else:
+                Q.update({("x"+str(i), "x"+str(i)):alpha*c[i-n]**2})
+        elif i > j:
+            if (i < n) & (j < n):
+                Q.update({("x"+str(i), "x"+str(j)):2*alpha*weight[i]*weight[j]})
+            elif (i >= n) & (j < n):
+                Q.update({("x"+str(i), "x"+str(j)):2*alpha*c[i-n]*weight[j]})
+            else:
+                Q.update({("x"+str(i), "x"+str(j)):2*alpha*c[i-n]*c[j-n]})
+        else:
+            continue
+
+response = EmbeddingComposite(DWaveSampler(token=setting.tokencode)).sample_qubo(Q, num_reads=3000)
+
+print(time.time() - start)
+
+if detail == True:
+
+    df_result = pd.DataFrame()
+    k = 0
+    for sample, energy, num_occurrences, chain_break_fraction in list(response.data()):
+        #print(sample, "Energy: ", energy, "Occurrences: ", num_occurrences)
+        df_tmp = pd.DataFrame(dict(sample), index=[k])
+        df_tmp['Energy'] = -energy
+        df_tmp['Occurrences'] = num_occurrences
+        df_result = df_result.append(df_tmp)
+        k+=1
+    
+    result = df_result.pivot_table(index=df_result.columns[:n].tolist()+['Energy'], values=['Occurrences'], aggfunc='sum').sort_values('Energy', ascending=False)
+        
+    print(result)
+    
+else:
+    print(list(response.data())[0])
+
+print("Total_real_time ", response.info["timing"]["total_real_time"], "us")
+print(time.time() - start)
